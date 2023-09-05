b91aa1b8976d1c0980fff28461662fcd884d3885
Abderrahmane369 <cszamasu@gmail.com>
2023-09-05 19:47:27 +0100

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for _ in range(100):
-    print("{}{}".format(_, ", " if _ != 99 else ""), end = "" if _ != 99 else "\n")
+    print("{:02}{}".format(_, ", " if _ != 99 else ""), end = "" if _ != 99 else "\n")
