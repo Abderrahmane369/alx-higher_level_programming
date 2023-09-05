53e1823fd4e37e1392a06379a4a18ca07a68e2a2
Abderrahmane369 <cszamasu@gmail.com>
2023-09-05 22:32:23 +0100

    12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for _ in range(1, 101):
+        if _ % 3 == _ % 5 == 0:
+            print("{}".format("FizzBuzz"), end=" ")
+        elif not _ % 3:
+            print("{}".format("Fizz"), end=" ")
+        elif not _% 5:
+            print("{}".format("Buzz"), end=" ")
+        else:
+            print("{}".format(_), end=" ")
+
+fizzbuzz()
