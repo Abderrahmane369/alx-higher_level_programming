c8daca0fef529a9f38c580e639ad047a088dbe9e
Abderrahmane369 <cszamasu@gmail.com>
2023-09-05 03:17:25 +0100

    10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,20 +8,32 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *h = list;
+	listint_t *h = list, *j = h;
 
-	if (!list)
-		return (0);
+	if (list)
+        h = h->next;
 
-	h = h->next;
-
-	while (h)
+    while (h)
 	{
 		if (h == list)
 			return (1);
 
 		h = h->next;
 	}
+
+    while (h)
+    {
+        while (j)
+        {
+            if (j->next == h->next && j != h)
+                return (0);
+            j = j->next;
+        }
+        
+        h = h->next;
+    }
+
+
 
 	return (0);
 }
