b62babcfd6638c6ef9417697d32870430cd73665
Abderrahmane369 <cszamasu@gmail.com>
2023-09-05 21:15:14 +0100

    8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for _ in range(len(str)):
+        if (ord(str[_]) >= 97 and ord(str[_]) < 97 + 26):
+            print("{}".format(chr(ord(str[_]) - 32)), 
+                  end="" if _ != len(str) - 1 else "\n")
+        else:
+            print("{}".format(str[_]), 
+                  end="" if _ != len(str) - 1 else "\n")
