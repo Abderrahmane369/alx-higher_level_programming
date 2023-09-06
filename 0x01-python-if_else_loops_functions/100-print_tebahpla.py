923c7352272e1390f98ac768aaf5ee1645615734
Abderrahmane369 <cszamasu@gmail.com>
2023-09-06 01:48:19 +0100

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for _ in range (26):
-    print("{}".format(chr((122 - 90) * ((_ + 1) % 2)+ 90 - _)), end="")
+for _ in range(26):
+    print("{}".format(chr((122 - 90) * ((_ + 1) % 2) + 90 - _)), end="")
