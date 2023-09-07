e9c6c08eb9c9f799ab848282569b4ed0ef73ead1
Abderrahmane369 <cszamasu@gmail.com>
2023-09-07 19:05:06 +0100

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def main():
+    from sys import argv
+
+    print("{} {}{}".format(
+        len(argv) - 1,
+        "argument" if len(argv) == 2 else "arguments",
+        ":" if len(argv) - 1 else "."
+        ))
+    
+    for _, arg in enumerate(argv[1:], start=1):
+        print("{}: {}".format(
+            _,
+            arg
+        ))
+    
+if __name__ == '__main__':
+    main()
