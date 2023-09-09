dfc6550f49c147f9afcb63d8f8ca2957641d6281
Abderrahmane369 <cszamasu@gmail.com>
2023-09-09 14:55:58 +0100

    0x03

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,3 +2,5 @@
 
 def print_list_integer(my_list=[]):
     [print("{}".format(_)) for _ in my_list]
+
+print_list_integer([5, 2, 1, 4, 6])
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+print(5, end="")
+print(6, end="")
