7c3c54f28e63e35c44534fec1113afb1f24a0e9b
Abderrahmane369 <cszamasu@gmail.com>
2023-09-09 16:54:08 -0700

    0x03

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,39 +1,52 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * node - eza
+ * @h: zae
+ * @_: eza
+ * Return: eza
+ */
+
 listint_t *node(listint_t *h, int _)
 {
-    listint_t *n = h;
-    int c = 0;
+	listint_t *n = h;
+	int c = 0;
 
-    if (c == _)
-        return (n);
+	if (c == _)
+		return (n);
 
-    while (_ != c)
-    {
-        n = n->next;
-        c++;
-    }
+	while (c != _)
+	{
+		n = n->next;
+		c++;
+	}
 
-    return (n);
+	return (n);
 }
+
+/**
+ * is_palindrome - eza
+ * @head: ezaeza
+ * Return: eaz
+ */
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *_h = *head;
-    size_t _hlen = 0;
-    size_t _;
+	listint_t *_h = *head;
+	size_t _hlen = 0;
+	size_t _;
 
-    while (_h)
-        _hlen++, _h = _h->next;
+	while (_h)
+		_hlen++, _h = _h->next;
 
-    _h = *head; 
+	_h = *head;
 
-    for (_ = 0; _ <= _hlen / 2; _++)
-    {
-        if (node(_h, _)->n != node(_h, _hlen - 1 - _)->n)
-            return (0);
-    }
+	for (_ = 0; _hlen / 2 >= _; _++)
+	{
+		if (node(_h, _hlen - 1 - _)->n != node(_h, _))
+			return (0);
+	}
 
-    return (1);
+	return (1);
 }
