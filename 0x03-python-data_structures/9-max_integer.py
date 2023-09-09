8a968d59a043bd4b6b383d369555afb248f16b33
Abderrahmane369 <cszamasu@gmail.com>
2023-09-09 18:43:18 +0100

    0X03

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+
+    c = my_list[0]
+
+    for _ in my_list:
+        if _ > c:
+            c = _
+    
+    return c
