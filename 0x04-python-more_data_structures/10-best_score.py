a3457acdef6fdec0f1c6f8d1efbff2dc12f1a7af
Abderrahmane369 <cszamasu@gmail.com>
2023-09-13 19:49:39 +0100

    0x04

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    return max(a_dictionary, key=lambda _: a_dictionary[_])
+    return max(a_dictionary, key=lambda _: a_dictionary[_]) if a_dictionary != {} else None
