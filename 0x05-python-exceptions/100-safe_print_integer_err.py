32437fdcfe471f265c8285070b060e0371b2a2e5
Abderrahmane369 <cszamasu@gmail.com>
2023-09-25 22:39:23 +0100

    0x05

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+
+    except Exception as _e:
+        print("Exception: {}".format(_e), file=stderr)
+
+        return False
+
+    return True
