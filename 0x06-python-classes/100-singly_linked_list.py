38d9ead1457f9a0101323cf1b8d1c24c884aa678
Abderrahmane369 <cszamasu@gmail.com>
2023-09-27 03:37:37 +0100

    0x06

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -7,29 +7,30 @@ class Node():
     def __init__(self, data, next_node=None):
         self._data = data
         self._next_node = next_node
-    
+
     @property
     def data(self):
         return self._data
-    
+
     @data.setter
     def data(self, _):
         if type(_) is not int:
             raise TypeError("data must be an integer")
-        
+
         self._data = _
-    
+
     @property
     def next_node(self):
         return self._next_node
-    
+
     @next_node.setter
     def next_node(self, node):
         if type(node) is not Node and node is not None:
             raise TypeError("next_node must be a Node object")
-        
+
         self._next_node = node
-    
+
+
 class SinglyLinkedList():
     """aaaaoeakzeaaoe"""
     def __init__(self):
@@ -37,7 +38,7 @@ class SinglyLinkedList():
 
     def __repr__(self) -> str:
         return "{}"
-    
+
     def sorted_insert(self, _):
         head = self._head
         prev = None
@@ -47,7 +48,4 @@ class SinglyLinkedList():
                 self._head.next_node = Node(_, self._head.next_node)
                 self._head = head
 
-
-            
             self._head = self._head.next_node
-
