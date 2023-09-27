58ed56b30e251624ef621d83290f60a49e16c691
Abderrahmane369 <cszamasu@gmail.com>
2023-09-27 01:18:43 +0100

    0x06

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,7 +6,7 @@ class Square():
     """bla bla bla
 
         Attributes:
-        _size (optional): size.
+        __size (optional): size.
     """
     def __init__(self, size):
-        self._size = size
+        self.__size = size
