e25b6a309c38be775ff74610c96fa0531dde7ffb
Abderrahmane369 <cszamasu@gmail.com>
2023-09-27 01:14:00 +0100

    0x06

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""blblablablabla"""
+
+
+class Square():
+    """bla bla bla
+
+        Attributes:
+        size (optional): size.
+    """
+    def __init__(self, size):
+        self._size = size
