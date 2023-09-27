f20c56c1da55735161e23ff2b342001179a3f26a
Abderrahmane369 <cszamasu@gmail.com>
2023-09-27 01:24:45 +0100

    0x06

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""blblablablabla"""
+
+
+class Square():
+    """bla bla bla
+
+        Attributes:
+        __size (int): size.
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+        if type(self.__size) is not int:
+            raise TypeError("size must be an integer")
+        
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
