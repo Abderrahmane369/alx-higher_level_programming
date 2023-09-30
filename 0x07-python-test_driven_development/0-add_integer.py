b68752621b89d8101617be7d82d74a4bcc724d6a
Abderrahmane369 <cszamasu@gmail.com>
2023-09-30 13:10:48 +0100

    0x07

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    Miami
+"""
+
+
+def add_integer(a, b=98):
+    """
+        DOES
+        :param a: aaaaa
+        :type a: aaa
+        :param b: bbbbb
+        :type b: bbbb
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
