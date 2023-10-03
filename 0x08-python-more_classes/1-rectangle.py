cc01e018bf8407b0b17bdf39845809a984be9511
Abderrahmane369 <cszamasu@gmail.com>
2023-10-03 22:32:57 +0100

    0x09

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""wooooooow"""
+
+
 class Rectangle():
+    """wooooooow"""
+
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
