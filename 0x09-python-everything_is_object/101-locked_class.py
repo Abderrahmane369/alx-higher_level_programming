eabbc02475a81ff15b00b3acd68e7f5df4f3d930
Abderrahmane369 <cszamasu@gmail.com>
2023-10-04 23:54:36 +0100

    0x09

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""lockde"""
+
+
+class LockedClass():
+    """looooo"""
+    def __setattr__(self, __name, __value) -> None:
+        if __name == "first_name":
+            super().__setattr__(__name, __value)
+
+        else:
+            raise AttributeError
