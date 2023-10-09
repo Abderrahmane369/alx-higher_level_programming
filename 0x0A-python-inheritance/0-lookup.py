491cdd675c49dd6dce927ebc6cb623dc54fc99ef
Replit user <>
2023-10-09 23:53:23 +0000

    a

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,17 +3,6 @@
 
 
 def lookup(obj):
-<<<<<<< HEAD
-     """return list of available attributes"""
-     attr = dir(obj)
-<<<<<<< HEAD
-     return attr
-=======
-     return attr
-=======
-	"""return list of available attributes"""
-
+     """return lg
+     ist of available attributes"""
 	return dir(obj)
->>>>>>> refs/remotes/origin/main
-
->>>>>>> f9d42b2f50ff59f1d1bcfc0d656d5bda9873e02fgit 
