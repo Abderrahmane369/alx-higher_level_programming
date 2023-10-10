0505183efaeec533b82e11e81a82917d1d2c2176
Abderrahmane369 <cszamasu@gmail.com>
2023-10-10 15:50:00 -0700

    0x0B

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """lsddqds"""
     with open(filename, encoding="utf-8") as f:
-      for line in f:
-        print(line, end="")
+        for line in f:
+            print(line, end="")
