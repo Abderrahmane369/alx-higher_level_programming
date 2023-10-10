a7cae245c4e822252c3ae7427b65a2742de2d575
Replit user <>
2023-10-10 22:30:05 +0000

    0x0B

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""looazoezaeaze"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """azeazeazeaz"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)
+        self.__size = size
+
+         
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+#
