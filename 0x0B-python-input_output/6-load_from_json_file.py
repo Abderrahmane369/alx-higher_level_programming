8cfc0a2e7abb8866d6d5db1001814901ac1181c3
Replit user <>
2023-10-10 23:36:21 +0000

    0x0B

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""module"""
+import json
+
+
+def load_from_json_file(filename):
+     """loads from json file"""
+     with open(filename, encoding="utf-8") as f:
+          return json.loads(f.read())
