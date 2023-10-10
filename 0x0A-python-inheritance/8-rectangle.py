79cc4a1a193f74d89abe869474abedd018fa490d
Replit user <>
2023-10-10 02:30:15 +0000

    0

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -18,8 +18,8 @@ class BaseGeometry():
 class Rectangle(BaseGeometry):
     """azeazeaea"""
     def __init__(self, width, height):
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
         self.__width = width
         self.__height = height
