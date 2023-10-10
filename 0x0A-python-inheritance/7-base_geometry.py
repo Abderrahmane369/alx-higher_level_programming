1a0989191e84920545dc941876aee2211c9711d7
Replit user <>
2023-10-10 01:44:55 +0000

    0x0A

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,7 +5,7 @@
 class BaseGeometry():
     """azeazeaea"""
     def area(self):
-        raise Exception("area() is not implemented")*
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if type(value) is not int:
