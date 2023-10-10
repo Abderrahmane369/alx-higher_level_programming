c12352f4941e70b298f41992edcbe1c8206e95e5
Abderrahmane369 <cszamasu@gmail.com>
2023-10-09 19:55:13 -0700

    0x0A

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -30,15 +30,16 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
+
 class Square(Rectangle):
-     """azeazeazeaz"""
-     def __init__(self, size):
-          self.integer_validator("size", size)
+    """azeazeazeaz"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
 
-          self.__size = size
+        self.__size = size
 
-     def area(self):
+    def area(self):
         return self.__size ** 2
 
-     def __str__(self):
+    def __str__(self):
         return "[Rectangle] {}/{}".format(self.__size, self.__size)
