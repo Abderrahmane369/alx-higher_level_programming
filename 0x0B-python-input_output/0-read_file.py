a932989f60c9fbee6c5e3e005c6d1e7707ae7be5
Abderrahmane369 <cszamasu@gmail.com>
2023-10-10 15:40:32 -0700

    0x0B

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,11 +3,5 @@
 
 
 def read_file(filename=""):
-<<<<<<< HEAD
     with open(filename, "r") as f:
         print(f.read())
-=======
-     """helloooo"""
-     with open(filename, "r") as f:
-         print(f.read())
->>>>>>> b66b9fb98f9cd128eb3bcfb7aac8db0d42d1979d
