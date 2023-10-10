e350c4da3cad23c3d572c1c980c7a0b034758fb9
Replit user <>
2023-10-10 23:54:29 +0000

    0x0B

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module"""
+import json, sys
+
+loadJSON = __import__('6-load_from_json_file').load_from_json_file
+saveJSON = __import__('5-save_to_json_file').save_to_json_file
+
+l = loadJSON('add_item.json')
+
+for _ in sys.argv:
+     l = l if type(l) is list else []
+     l.append(_)
+
+saveJSON(l, 'add_item.json')
