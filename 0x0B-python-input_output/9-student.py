81b8b6fffa9c24508d603a84582bd0f524178de8
Abderrahmane369 <cszamasu@gmail.com>
2023-10-10 18:14:51 -0700

    0x0B

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,11 +3,11 @@
 
 
 class Student():
-     """zaazeazea"""
-     def __init__(self, first_name, last_name, age):
-          self.first_name = first_name
-          self.last_name = last_name
-          self.age = age
+    """zaazeazea"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-     def to_json(self):
-          return self.__dict__
+    def to_json(self):
+        return self.__dict__
