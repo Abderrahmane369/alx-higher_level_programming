ea9ef01b601e019156e373ffc02b4422a6876468
Abderrahmane369 <cszamasu@gmail.com>
2023-10-10 19:51:22 -0700

    0x0B

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,18 +3,19 @@
 
 
 def pascal_triangle(n):
-     """pascallllll"""
-     if n <= 0:
-          return []
+    """pascallllll"""
+    if n <= 0:
+        return []
 
-     arr = [[1]]
+    arr = [[1]]
 
-     for _ in range(n - 1):
-          col = [1]
-          
-          for n in range(1, len(arr[-1])):
-               col += [arr[-1][n] + arr[-1][n - 1]]
-          col += [1]
-          arr += [col]
+    for _ in range(n - 1):
+        col = [1]
 
-     return arr
+        for n in range(1, len(arr[-1])):
+            col += [arr[-1][n] + arr[-1][n - 1]]
+
+        col += [1]
+        arr += [col]
+
+    return arr
