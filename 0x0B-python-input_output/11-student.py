c06e050c7ee601f98235c7bfee2d1887fd4b6d29
Replit user <>
2023-10-11 01:40:44 +0000

    0x0B

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""azeazezaezaeaea"""
+
+
+class Student():
+    """zaazeazea"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            return {_: v for _, v in self.__dict__.items() if _ in attrs}
+
+        return self.__dict__
+     
+    def reload_from_json(self, json):
+         for _, v in json.items():
+              setattr(self, _, v)
