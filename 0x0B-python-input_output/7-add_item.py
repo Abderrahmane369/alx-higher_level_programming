e2241b35fb81564e2fb057b31b901b4839d22f94
Replit user <>
2023-10-11 01:02:11 +0000

    0x0B

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,16 +5,11 @@ import sys
 loadJSON = __import__('6-load_from_json_file').load_from_json_file
 saveJSON = __import__('5-save_to_json_file').save_to_json_file
 
+try:
+     loaded = loadJSON('add_item.json')
+except json.decoder.JSONDecodeError:
+     loaded = []
 
-def main():
-    """zaeaeazeaz"""
-    loaded = loadJSON('add_item.json')
+loaded += [sys.argv[1:]]
 
-    for _ in sys.argv[1:]:
-        loaded.append(_)
-
-    saveJSON(l, 'add_item.json')
-
-
-if __name__ == '__main__':
-    main()
+saveJSON(l, 'add_item.json')
---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""azeazezaezaeaea"""
+
+
+def class_to_json(obj):
+     """zaezaeazeaz"""
+     
