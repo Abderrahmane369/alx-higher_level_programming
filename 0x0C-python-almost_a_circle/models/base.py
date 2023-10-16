b1a11117472a62fb9885617fec04e1fc6467893e
Abderrahmane369 <cszamasu@gmail.com>
2023-10-17 00:25:28 +0100

    0x0C

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -69,8 +69,7 @@ class Base():
         Args:
             **dictionary: double pointer to a dictionary.
         """
-        dummy = cls(1)
-        dummy = cls(1, 1) if cls.__name__ == 'Rectangle' else dummy
+        dummy = cls(1, 1) if cls.__name__ == 'Rectangle' else cls(1)
         dummy.update(**dictionary)
 
         return dummy
