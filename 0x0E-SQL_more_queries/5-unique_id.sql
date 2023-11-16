6ce5e2cb2f570ecea72098f1354ae6137310e1a5
Abderrahmane369 <cszamasu@gmail.com>
2023-11-16 01:06:01 +0100

    New_update

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,8 @@
 -- TASK 5
 -- BEGINS
 CREATE TABLE IF NOT EXIST unique_id (
-				id INT DEFAULT 1 UNIQUE,
-				name VARCHAR(256)
+				id INT DEFAULT 1,
+				name VARCHAR(256),
+				UNIQUE (id)
 );
 
