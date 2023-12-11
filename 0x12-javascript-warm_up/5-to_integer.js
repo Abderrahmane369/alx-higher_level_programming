57501c5a2238f98e4d1f2a5ca95745c688942cc6
Abderrahmane369 <cszamasu@gmail.com>
2023-12-11 22:11:48 +0100

    JS

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const argv = process.argv;
 if (isNaN(parseInt(argv[2]))) {
   console.log('Not a number');
 } else {
-  console.log(parseInt(argv[2]));
+  console.log(parseInt('My number: ' + argv[2]));
 }
