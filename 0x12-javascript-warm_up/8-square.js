aac7580e3e64f5e273c12d1600d1a3562b4fdbf5
Abderrahmane369 <cszamasu@gmail.com>
2023-12-11 22:33:36 +0100

    JS

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,9 @@
 const argv = process.argv;
 
 if (isNaN(parseInt(argv[2]))) {
-				console.log('Missing size');
+  console.log('Missing size');
 } else {
-				for (let i = 0; i < argv[2]; i++) {
-								console.log('#'.repeat(argv[2]));
-				}
+  for (let i = 0; i < argv[2]; i++) {
+    console.log('X'.repeat(argv[2]));
+  }
 }
