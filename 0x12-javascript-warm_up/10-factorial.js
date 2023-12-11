1a4495b5bf6b5ef246ea817205f008867c58ae2e
Abderrahmane369 <cszamasu@gmail.com>
2023-12-11 22:40:33 +0100

    JS

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+function factorial (n) {
+				if (n === 0 || n === 1 || isNaN(n)) return 1;
+
+				return n * factorial(n - 1);
+}
+
+console.log(factorial(parseInt(argv[2])));
+
