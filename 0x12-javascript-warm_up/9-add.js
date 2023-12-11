a93e129147ca2fb54d67e8aa4641fc55eafb648a
Abderrahmane369 <cszamasu@gmail.com>
2023-12-11 22:36:29 +0100

    JS

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,9 +2,8 @@
 
 const argv = process.argv.splice(2);
 
-function add(a, b) {
-				return parseInt(a) + parseInt(b);
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
 }
 
 console.log(add(argv[0], argv[1]));
-
