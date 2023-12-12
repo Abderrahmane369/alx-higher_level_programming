16fbfb1b40cd3113c7e60f9bf8119ff29986b0ec
Abderrahmane369 <cszamasu@gmail.com>
2023-12-12 22:43:37 +0100

    JS

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,9 +3,6 @@
 exports.esrever = function (list) {
   const arr = [];
 
-  for (const i in list) {
-    arr[i] = list.at(-i - 1);
-    console.log(arr[i]);
-  }
+  for (const i in list) arr[i] = list.at(-i - 1);
   return arr;
 };
