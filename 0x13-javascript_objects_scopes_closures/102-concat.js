31fc83b39c43ecf59eefe1b36a141257867b2afe
Abderrahmane369 <cszamasu@gmail.com>
2023-12-13 01:15:54 +0100

    JS

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+const fs = require("fs");
 const argv = process.argv.splice(2);
 
-const fileA = fs.readFileSync(argv[0], 'utf8');
-const fileB = fs.readFileSync(argv[1], 'utf8');
-fs.writeFile(argv[2], fileA + fileB, (err) => (err || null));
+const fileA = fs.readFileSync(argv[0], "utf8");
+const fileB = fs.readFileSync(argv[1], "utf8");
+fs.writeFile(argv[2], fileA + fileB, (err) => err || null);
