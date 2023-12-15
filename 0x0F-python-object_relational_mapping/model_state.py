ebb752768f0ddf1389a59f19dc3bc29edd7c8636
Abderrahmane369 <cszamasu@gmail.com>
2023-12-15 23:11:51 +0100

    SQLxPython

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module that contains the clasand an instance"""
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -10,9 +10,5 @@ class State(Base):
     """LOOOOL"""
     __tablename__ = "states"
     id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False, unique=True, index=True)
+                primary_key=True, nullable=False, unique=True)
     name = Column("name", String(128), nullable=False)
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
