45957ccbe713ef43b868922150e53e1c8e9c777a
Abderrahmane369 <cszamasu@gmail.com>
2023-12-15 23:05:37 +0100

    SQLxPython

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Module that contains the clasand an instance"""
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
