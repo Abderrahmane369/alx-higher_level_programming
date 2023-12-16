dc80b89cc7a2df1ed192b52346b50b5a11c2e3dd
Abderrahmane369 <cszamasu@gmail.com>
2023-12-16 01:43:35 +0100

    SQLxPython

---
+++ 0x0F-python-object_relational_mapping/class.py
@@ -0,0 +1,25 @@
+
+
+# class Vehicle():
+
+#     def __init__(self, speed, seat):
+#         self.speed = speed
+#         self.seat = seat
+
+# class Car(Vehicle);
+#     def __init__(self, speed, seat):
+#         super().__init__(speed, seat)
+#         self.COLOR
+
+# myCar = Car('Blue', 'Peugot', 'ZAOPE2K93EJO02')
+
+# myCar.color
+
+# v = "jawad"
+# for x in v:
+#     print(x)
+
+a = "zaekoapea"
+
+for i in a:
+    print(i * 2)
