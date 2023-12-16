35d7c61a2eb02c9f072e3640cc2e9bde02e7ea97
Abderrahmane369 <cszamasu@gmail.com>
2023-12-16 02:34:18 +0100

    SQLxPython

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module that contains the clasand an instance"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                    argv[2], argv[3]),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    updated = session.query(State).filter(State.id == 2).first()
+    updated.name = "New Mexico"
+
+    session.commit()
+
+
+if __name__ == '__main__':
+    main()
