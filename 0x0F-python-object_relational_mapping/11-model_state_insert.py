41168d6323dd00f230a1da94cbdd57218c7c740b
Abderrahmane369 <cszamasu@gmail.com>
2023-12-16 02:27:40 +0100

    SQLxPython

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that contains the clasand an instance"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                    argv[2], argv[3]),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    louisana = State()
+    louisana.name = "Louisiana"
+
+    session.add(louisana)
+    session.commit()
+
+    print(louisana.id)
+
+if __name__ == '__main__':
+    main()
