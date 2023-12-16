2018de407df9a037cd421d64d86d6c3c6231f436
Abderrahmane369 <cszamasu@gmail.com>
2023-12-16 02:16:14 +0100

    SQLxPython

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that contains the clasand an instance"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def main():
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                    argv[2], argv[3]),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    obj = session.query(State).filter(State.name == argv[4])
+
+    if obj[0]:
+        print(obj[0].id)
+    else:
+        print("Not found")
+
+
+if __name__ == '__main__':
+    main()
