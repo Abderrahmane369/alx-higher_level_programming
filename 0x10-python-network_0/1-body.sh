05dd3581a844d13e926a2dbd44dd84b163f034f3
Abderrahmane369 <cszamasu@gmail.com>
2024-01-05 21:04:59 +0100

    Updated_Something

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 #aa
-url="$1"
-
-# Use curl to get the body and status code of the response
-response=$(curl -s -w "%{http_code}" "$url")
-
-# Extract status code
-http_status=$(echo "$response")
-
-# Check if the status code is 200 (OK)
-if [ "$http_status" -eq 200 ]; then
-  # Display the body of the response
-  curl -s "$url"
-fi
+curl -s -w "%{http_code}\n" $1
