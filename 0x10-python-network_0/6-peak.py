0dacc7de9bc9f156317b230fba46e8ca6f8b3748
Abderrahmane369 <cszamasu@gmail.com>
2024-01-05 22:18:55 +0100

    Updated_Something

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Ubunatu Misogashi"""
+
+def find_peak(list_of_integers):
+    """_summary_
+
+    Args:
+        list_of_integers (_type_): _description_
+    """
+    return max(list_of_integers)
