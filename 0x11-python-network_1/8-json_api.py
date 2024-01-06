58fe93ae73eaa3282dcbbbfc6f9112ffbc847f32
Abderrahmane369 <cszamasu@gmail.com>
2024-01-06 21:06:59 +0100

    Updated_Something

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""oooiiuuuiiooo"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    rqst = requests.post(argv[1], data={
+        "q": argv[2]
+    })
+
+    print(rqst.content.decode())
