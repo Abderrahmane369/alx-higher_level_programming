b986868287864793816dcbe9e957424f9b33797a
Abderrahmane369 <cszamasu@gmail.com>
2024-01-06 17:50:10 +0100

    Updated_Something

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import urllib.request as rqst
+import urllib.parse as parse
+from sys import argv
+
+if __name__ == "__main__":
+    parsedData = parse.urlencode({
+        "email": argv[2]
+    }).encode()
+
+    with rqst.urlopen(rqst.Request(argv[1],
+                                   data=parsedData, method="POST")) as rsp:
+        print(rsp.read().decode())
