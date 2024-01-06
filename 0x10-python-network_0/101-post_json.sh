2841ae947466cb4129f13e7925f165d0420323cc
Abderrahmane369 <cszamasu@gmail.com>
2024-01-06 02:08:35 +0100

    Updated_Something

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #opinui
-curl -s -X POST -H "Content-Type: application/json" --data @"request.json" "0.0.0.0:5000/route_json"
-
+curl -s -X POST -H "Content-Type: application/json" --data @"$2" "$1"
