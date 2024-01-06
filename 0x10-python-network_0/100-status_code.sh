becfbfe775edbc7851c5fe91d5e7c5cae16a48c1
Abderrahmane369 <cszamasu@gmail.com>
2024-01-06 01:38:43 +0100

    Updated_Something

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
 #loazeo
 curl -s -o /dev/null -w "%{http_code}" "$1"
-=======
-#binobasha
-curl -o /dev/null -s -w "%{http_code}\n" "$1"
-
->>>>>>> refs/remotes/origin/main
