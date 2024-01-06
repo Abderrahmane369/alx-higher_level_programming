55e63afe12f53d98e13235eb4b620110ec55f17e
Abderrahmane369 <cszamasu@gmail.com>
2024-01-06 02:12:42 +0100

    Updated_Something

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 #lloeazo
-
-curl -X POST -H "Content-Type: application/json" -d "@$2" $1
-#!/bin/bash
-#lloeazo
-
-response=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "@$2" $1)
-echo $response
+curl -s -X POST -H "Content-Type: application/json" --data @"$2" "$1"
