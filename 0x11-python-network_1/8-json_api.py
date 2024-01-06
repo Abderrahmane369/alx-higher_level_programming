49e6acd83cbe28a011676657a48e32db630a9ae3
Abderrahmane369 <cszamasu@gmail.com>
2024-01-06 21:39:30 +0100

    Updated_Something

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,10 +2,21 @@
 """oooiiuuuiiooo"""
 import requests
 from sys import argv
+import json
 
 if __name__ == "__main__":
-    rqst = requests.post(argv[1], data={
-        "q": argv[2]
+    rqst = requests.post("http://0.0.0.0:5000/search_user", data={
+        "q": argv[1] if len(argv) == 2 else ""
     })
 
-    print(rqst.content.decode())
+    try:
+        if json.loads(rqst.content.decode()) == {}:
+            print("No result")
+
+        else:
+            print("[{}] {}".format(
+                json.loads(rqst.content.decode()).get('id'),
+                json.loads(rqst.content.decode()).get('name')
+            ))
+    except ValueError:
+        print("Not a valid JSON")
