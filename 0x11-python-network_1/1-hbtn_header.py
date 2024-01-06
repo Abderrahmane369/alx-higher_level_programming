443faf94490c1a09539482789f382b1c0d727e2c
Abderrahmane369 <cszamasu@gmail.com>
2024-01-06 16:19:16 +0100

    Updated_Something

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """loooogarithm"""
 import urllib.request as rqst
-from sys import argv as args
+import sys
 
-with rqst.urlopen(args[1]) as rsp:
+with rqst.urlopen(sys.argv[1]) as rsp:
     con = rsp
 
 print(dict(con.getheaders()).get("X-Request-Id"))
