36700a87942de65f1c87f18fb8c8549a94b1a556
Abderrahmane369 <cszamasu@gmail.com>
2024-01-07 18:25:47 +0100

    Updated_Something

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,7 @@
 from requests import get
 
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    response = get(url)
+    response = get('https://intranet.hbtn.io/status')
     bytes_content = response.text
     string = f'Body response:\n\t- type: {type(bytes_content)}\n\t- content: {bytes_content}'
     print(string)
