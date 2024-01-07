49e9d2786a3012e64170a61515db99a922797e98
Abderrahmane369 <cszamasu@gmail.com>
2024-01-07 18:05:02 +0100

    Updated_Something

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ozeàofibog"""
-import requests
+from requests import get
 
 if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+    r = get("https://alx-intranet.hbtn.io/status")
 
     print(f"Body response:\n\t- type: {type(r.text)}\n\t- content: {r.text}")
