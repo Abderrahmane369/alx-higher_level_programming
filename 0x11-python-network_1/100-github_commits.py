f284c37714165692ce4ee7a2282f7fcb3e1f5091
Abderrahmane369 <cszamasu@gmail.com>
2024-01-07 17:10:19 +0100

    Updated_Something

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,7 @@ if __name__ == "__main__":
     rq = requests.get("https://api.github.com/repos/rails/rails/commits",
                       params={'sort': 'committer-date-desc'},
                       auth=(argv[1], argv[2]))
-    
+
     for _ in rq.json()[:10]:
         print(_.get("commit").get("tree").get("sha"), end=": ")
         print(_.get("commit").get("author").get("name"), end="\n\n")
-
