72efc2decb380b35d1bb14d4b58d1b5edf005358
Abderrahmane369 <cszamasu@gmail.com>
2024-01-30 23:09:26 +0100

    Updated_Something

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const argv = process.argv.splice(2);
 
 request(argv[0], (err, res, body) => {
-  if (err) return;
+  if (err) console.log(err);
   fs.writeFile(argv[1], body, err => {
-    if (err) return;
+    if (err) console.log(err);
   });
 });
