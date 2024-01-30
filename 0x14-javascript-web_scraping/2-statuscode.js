93400a62357498066ff756eeb9208ebae3cb9d2b
Abderrahmane369 <cszamasu@gmail.com>
2024-01-30 22:20:36 +0100

    Updated_Something

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,6 @@
 const request = require('request');
 
 request(process.argv.splice(2)[0], (err, res, body) => {
+  if (err) console.log(`code: ${res.statusCode}`);
   console.log(`code: ${res.statusCode}`);
 });
