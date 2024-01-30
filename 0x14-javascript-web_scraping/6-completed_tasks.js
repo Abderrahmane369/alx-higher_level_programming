d77bd56ee439f012ba951419da60e062a7177961
Abderrahmane369 <cszamasu@gmail.com>
2024-01-30 23:39:19 +0100

    Updated_Something

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,16 +4,16 @@ const request = require('request');
 request(process.argv.splice(2)[0], (err, res, body) => {
   if (err) console.log(err);
 
-  data = JSON.parse(body);
+  let data = JSON.parse(body);
   data = data.filter(e => {
     return e.completed;
   });
 
-  let userTask = {};
+  const userTask = {};
 
   for (let i = 1; i <= 10; i++) {
-    let tArrLen = data.filter(e => {
-      return e.userId == i;
+    const tArrLen = data.filter(e => {
+      return e.userId === i;
     }).length;
 
     userTask[i.toString()] = tArrLen;
